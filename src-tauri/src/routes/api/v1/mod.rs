eafaf0c803242ed2a41ab0608ebfdfc2a1c2938b
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-02-01 17:44:26 +0700

    Remove unused routes from api

--- src-tauri/src/routes/api/v1/mod.rs
+++ src-tauri/src/routes/api/v1/mod.rs
@@ -1,13 +1,9 @@
-mod convert;
 mod convert_raw;
 mod query;
 
 use axum::{routing::post, Router};
-use convert::convert;
 use convert_raw::convert_raw;
 
 pub fn create_v1_routes() -> Router {
-    Router::new()
-        .route("/convert", post(convert))
-        .route("/convert/raw", post(convert_raw))
+    Router::new().route("/convert/raw", post(convert_raw))
 }
--- src-tauri/src/routes/mod.rs
+++ src-tauri/src/routes/mod.rs
@@ -1,7 +1,7 @@
 mod api;
 
-use api::{create_api_routes, v1::create_v1_routes};
-use axum::{http::Method, response::Redirect, routing::get, Router};
+use api::create_api_routes;
+use axum::{http::Method, Router};
 use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router {
@@ -9,15 +9,7 @@ pub fn create_routes() -> Router {
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
 
-    let v1 = create_v1_routes();
     let api = create_api_routes();
 
-    Router::new()
-        .route(
-            "/",
-            get(|| async { Redirect::permanent("https://github.com/tapciify/api") }),
-        )
-        .nest("/", v1)
-        .nest("/api", api)
-        .layer(cors)
+    Router::new().nest("/api", api).layer(cors)
 }
