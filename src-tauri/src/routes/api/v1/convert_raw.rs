7131f7b2aadec7ee77658f88593073ab319b803a
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-02-14 09:55:35 +0700

    Update tapciify (#15)

--- src-tauri/src/routes/api/v1/convert_raw.rs
+++ src-tauri/src/routes/api/v1/convert_raw.rs
@@ -3,12 +3,15 @@ use axum::{
     extract::{Multipart, Query},
     Json,
 };
-use image::io::Reader as ImageReader;
+use image::{imageops::FilterType, io::Reader as ImageReader};
 use serde::Serialize;
 use std::io::Cursor;
-use tapciify::{AsciiConverter, RawAsciiArt, DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO};
+use tapciify::{
+    AsciiArt, AsciiArtConverter, AsciiArtConverterOptions, AsciiArtPixel, CustomRatioResize,
+    DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO,
+};
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug, Clone)]
 pub struct AsciiCharacterDef {
     pub character: char,
     pub r: u8,
@@ -17,14 +20,36 @@ pub struct AsciiCharacterDef {
     pub a: u8,
 }
 
-#[derive(Serialize)]
+impl From<AsciiArtPixel> for AsciiCharacterDef {
+    fn from(p: AsciiArtPixel) -> AsciiCharacterDef {
+        AsciiCharacterDef {
+            character: p.character,
+            r: p.r,
+            g: p.g,
+            b: p.b,
+            a: p.a,
+        }
+    }
+}
+
+#[derive(Serialize, Debug, Clone)]
 pub struct RawAsciiArtDef {
     pub characters: Vec<AsciiCharacterDef>,
     pub width: u32,
     pub height: u32,
 }
 
-#[derive(Serialize)]
+impl From<AsciiArt> for RawAsciiArtDef {
+    fn from(a: AsciiArt) -> RawAsciiArtDef {
+        RawAsciiArtDef {
+            characters: a.characters.iter().map(|c| c.to_owned().into()).collect(),
+            width: a.width,
+            height: a.height,
+        }
+    }
+}
+
+#[derive(Serialize, Debug, Clone)]
 pub struct ConvertRawResult {
     pub data: Vec<RawAsciiArtDef>,
 }
@@ -33,7 +58,7 @@ pub async fn convert_raw(
     query: Query<ConvertQuery>,
     mut multipart: Multipart,
 ) -> Json<ConvertRawResult> {
-    let mut raw_ascii_images: Vec<RawAsciiArt> = vec![];
+    let mut ascii_arts: Vec<AsciiArt> = vec![];
 
     while let Some(field) = multipart.next_field().await.unwrap() {
         let data = field.bytes().await.unwrap();
@@ -51,40 +76,30 @@ pub async fn convert_raw(
                 urlencoding::decode(&encoded).unwrap().into_owned()
             });
 
-        let ascii_converter = AsciiConverter {
-            img,
-            width: query.width.unwrap_or(0),
-            height: query.height.unwrap_or(0),
-            ascii_string: if query.reverse.unwrap_or(false) {
-                ascii_string.chars().rev().collect()
-            } else {
-                ascii_string
-            },
-            font_ratio: query.font_ratio.unwrap_or(DEFAULT_FONT_RATIO),
-            ..Default::default()
-        };
+        let ascii_art = img
+            .resize_custom_ratio(
+                query.width,
+                query.height,
+                query.font_ratio.unwrap_or(DEFAULT_FONT_RATIO),
+                FilterType::Triangle,
+            )
+            .ascii_art(&AsciiArtConverterOptions {
+                ascii_string: if query.reverse.unwrap_or(false) {
+                    ascii_string.chars().rev().collect()
+                } else {
+                    ascii_string
+                },
+                colored: true,
+            })
+            .unwrap();
 
-        raw_ascii_images.push(ascii_converter.convert_raw().unwrap());
+        ascii_arts.push(ascii_art);
     }
 
     Json(ConvertRawResult {
-        data: raw_ascii_images
+        data: ascii_arts
             .iter()
-            .map(|raw_ascii_image| RawAsciiArtDef {
-                characters: raw_ascii_image
-                    .characters
-                    .iter()
-                    .map(|ascii_character| AsciiCharacterDef {
-                        character: ascii_character.character,
-                        r: ascii_character.r,
-                        g: ascii_character.g,
-                        b: ascii_character.b,
-                        a: ascii_character.a,
-                    })
-                    .collect(),
-                width: raw_ascii_image.width,
-                height: raw_ascii_image.height,
-            })
+            .map(|ascii_art| ascii_art.to_owned().into())
             .collect(),
     })
 }
